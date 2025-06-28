c3e5db4e79f369ce544004ad59fbb109922da41a
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2025-06-28 14:48:13 +0700

    add factory

---
+++ database/factories/PublisherFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Publisher>
+ */
+class PublisherFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $name = fake()->company(),
+            'slug' => str()->lower(str()->slug($name) . str()->random(4)),
+            'address' => fake('id_ID')->address(),
+            'email' => fake('id_ID')->unique()->safeEmail(),
+            'phone' => fake('id_ID')->phoneNumber()
+        ];
+    }
+}
