495401075cf54f98f80aca381cb5ca1d48f881bb
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2025-08-13 00:17:38 +0700

    add error handling for production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Spatie\Permission\Middleware\RoleMiddleware;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,5 +25,16 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->respond(function (Response $response, Throwable $exceptions, Request $request) {
+            if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+                return Inertia::render('ErrorHandling', [
+                    'status' => $response->getStatusCode()
+                ])->toResponse($request)->setStatusCode($response->getStatusCode());
+            } elseif ($response->getStatusCode() === 419) {
+                return back()->with([
+                    'message' => 'The page is expired, please try again'
+                ]);
+            }
+            return $response;
+        });
     })->create();
