19dd12d045692bed291988abb6b863c7b77d65a8
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2025-08-15 15:42:43 +0700

    add format to rupiah

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -12,6 +12,7 @@ import { Button } from '@/Components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/ui/table';
 import AppLayout from '@/Layouts/AppLayout';
+import { formatToRupiah } from '@/lib/utils';
 import { Link } from '@inertiajs/react';
 
 export default function Dashboard(props) {
@@ -95,7 +96,7 @@ export default function Dashboard(props) {
               iconClassName: 'text-white',
             }}
           >
-            <div className="text-2xl font-bold">{props.page_data.total_fines}</div>
+            <div className="text-2xl font-bold">{formatToRupiah(props.page_data.total_fines)}</div>
           </CardStat>
         </div>
       )}
